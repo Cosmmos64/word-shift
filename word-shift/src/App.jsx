af14929046a1e077e7cabf73a746863716ad847c
cosmmos64 <cosmmos64@gmail.com>
2024-09-18 16:04:01 +0000

    commit #2

--- word-shift/src/App.jsx
+++ word-shift/src/App.jsx
@@ -3,19 +3,40 @@ import './App.css';
 import wordList from './wordlist.js';
 
 export default function App() {
-  const givenWord = randWord();
-  const givenWordTwo = randWord();
+  const list = shuffledList();
 
   return (
     <>
-      <h1>{givenWord} to {givenWordTwo}</h1>
-      <form>
+      <h1>{list}</h1>
+      <form onSubmit={handleSubmit()}>
         <input type="text" />
       </form>
+      <ul>
+        <li></li>
+      </ul>
     </>
   );
 }
+function handleSubmit() {
 
-function randWord() {
-  return wordList[Math.floor(Math.random() * wordList.length)];
+}
+
+function shuffledList() {
+  const randWord1 = wordList[Math.floor(Math.random() * wordList.length)];
+  const randWord2 = wordList[Math.floor(Math.random() * wordList.length)];
+  
+  // Combine the two random words into a single string
+  const combinedWords = randWord1 + randWord2;
+  
+  // Create an array from the combined words
+  const list = combinedWords.split('');
+
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  return shuffle(list);
 }
